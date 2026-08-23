765e4ba3dd166db9cf60eca9d2c61a76c8a4a7cc
Mohit <mohitchandrakanths7@gmail.com>
2026-08-23 13:59:19 +0530

    Add homepage route

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
+app.get('/', (req, res) => {
+  res.send('MARKETzo is live! 🚀');
+});
 const PORT = process.env.PORT || 5000;
 
 // Initialize Database
